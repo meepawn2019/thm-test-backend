d6949fc96e70099e85af55755a387578f938a201
meepawn2019 <meepawn2018@gmail.com>
2023-01-08 01:06:18 +0700

    remove auth

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { expressjwt: jwt } = require("express-jwt");
 const tokenHandler = require("../middleware/authorizationMiddleware");
 
 // ad the tokenHandler middleware to the users route
-router.use("/users", tokenHandler);
+// router.use("/users", tokenHandler);
 
 router.use("/users", usersRouter);
 router.use("/auth", authRouter);
